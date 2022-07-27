60e164b23bdbf1dcd8a50bf8528e2e212ea9ce39
robinbenitezmora <robinbenitez1990@gmail.com>
2022-07-27 08:48:43 -0500

    Add code lines in hmtl, css and javascript in order to give dinamism to 'mobile' version in its first stage

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ closeSlideMenu.addEventListener('click', () => {
 closeNav.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.remove('active');
 });
+
+// Open Capstone
+const openAbout = document.querySelector('.menu__topics');
+const openSlideMenuAbout = document.querySelector('.hamburger-menu__about');
+const returnMainPage = document.querySelector('.logo__slide-menu');
+
+openAbout.addEventListener('click', () => {
+  document.getElementById('about').classList.add('active');
+});
+
+openSlideMenuAbout.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.add('active');
+});
+
+returnMainPage.addEventListener('click', () => {
+  document.getElementById('about').classList.remove('active');
+});
+
+
+
